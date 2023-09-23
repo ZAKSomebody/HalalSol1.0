e191e6e6c3b32d398384e9919eed675974576bcb
Zain Ahmad Khan Sherwani <45851920+ZAKSomebody@users.noreply.github.com>
2023-09-23 14:36:58 +0500

    commented

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ start[0].addEventListener('click', (e) =>{
     load[0].style.width = 0;
     load[1].style.width = 0;
     start[0].innerHTML = "";
-    start[0].style.padding = 0; 
+    start[0].style.padding = 0;
 });
