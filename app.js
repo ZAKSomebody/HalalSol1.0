f5a00343c7f6a9e001de5fb157d18c535631d1ab
Zain Ahmad Khan Sherwani <45851920+ZAKSomebody@users.noreply.github.com>
2023-10-05 01:49:25 +0500

    stuff

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const start = document.getElementsByClassName("start");
-const load = document.getElementsByClassName("load");
-start[0].addEventListener('click', (e) =>{
-    load[0].style.width = 0;
-    load[1].style.width = 0;
-    start[0].innerHTML = "";
-    start[0].style.padding = 0;
-});
+// const start = document.getElementsByClassName("start");
+// const load = document.getElementsByClassName("load");
+// start[0].addEventListener('click', (e) =>{
+//     load[0].style.width = 0;
+//     load[1].style.width = 0;
+//     start[0].innerHTML = "";
+//     start[0].style.padding = 0;
+// });
