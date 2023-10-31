faa649818b8f5ea48b412139c1b9c036a0962a5a
Zain Ahmad Khan Sherwani <45851920+ZAKSomebody@users.noreply.github.com>
2023-10-31 23:45:34 +0500

    darkLight comment

--- app.js
+++ app.js
@@ -6,3 +6,22 @@
 //     start[0].innerHTML = "";
 //     start[0].style.padding = 0;
 // });
+
+// const r = document.querySelector(':root');
+// const rs = getComputedStyle(r);
+// const DL = document.getElementsByClassName("lightMode");
+// const DLD = getComputedStyle(DL[0]);
+// function darkLight() {
+//     if (DLD.backgroundColor === 'rgb(247, 247, 247)') {
+//         r.style.setProperty('--clrBlue', 'darkblue');
+//         r.style.setProperty('--clrPurple', 'purple');
+//         r.style.setProperty('--clrBlack', 'rgb(247, 247, 247)');
+//         r.style.setProperty('--clrWhite', 'rgb(3, 3, 3)');
+//     }
+//     else{
+//         r.style.setProperty('--clrBlue', 'rgb(113, 97, 239)');
+//         r.style.setProperty('--clrPurple', 'rgb(149, 127, 239)');
+//         r.style.setProperty('--clrBlack', 'rgb(3, 3, 3)');
+//         r.style.setProperty('--clrWhite', 'rgb(247, 247, 247)');
+//     }
+// }
